24c4bfc792d38e859c8714906ba20a17b9caf3a4
Joaquin Ruiz Luque <joaquinruizluque@gmail.com>
2018-10-10 23:39:11 -0300

    First commit

---
+++ spring-boot-thymeleaf/src/main/java/com/bolsadeideas/springboot/app/controllers/IndexController.java
@@ -0,0 +1,20 @@
+package com.bolsadeideas.springboot.app.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+	@Value("${application.controllers.mensaje}")
+	private String mensaje;
+	
+	@GetMapping(value="/")
+	public String hola(Model model) {
+	
+		model.addAttribute("mensaje", mensaje);
+		return "hola";
+	}
+}
